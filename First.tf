0467e230e588d48bd771788166adf0af8439381d
Shubham <byshubham6@gmail.com>
2024-08-01 20:51:00 +0530

    #

---
+++ First.tf
@@ -0,0 +1,10 @@
+# Terraform Configuration Block Syntax
+
+// Define a block with "label1" and "label2"
+//      label1 "label2" {
+//           identifier = expression
+//         } 
+
+output hello { 
+    value = "Hello Shubhm"
+}
---
+++ Multiple_block.tf
@@ -0,0 +1,13 @@
+# Multiple Block in single terraform file
+
+output "Block1" {
+  value = "this is first hello world block"
+}
+
+output "Block2" {
+  value = "this is second hello world block"
+}
+
+output "Block3" {
+  value = "this is third hello world block"
+}
