fed5feb6f087ecb1181b5e627120d7a20e5ee076
Shubham <byshubham6@gmail.com>
2024-08-01 21:36:28 +0530

    #

--- Hello_variable.tf
+++ Hello_variable.tf
@@ -1,3 +1,7 @@
+# It looks like you're working with Terraform and defining a variable and an output.
+// 1.Define the variable with a default value (if desired).
+// 2.Reference the variable correctly in the output.
+
 variable "username" {
   
 }
