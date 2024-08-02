20fdf784786b20c2eb85202bd72696a22205e1a8
Shubham <byshubham6@gmail.com>
2024-08-02 09:53:19 +0530

    #

--- 4.Multiple_variable.tf
+++ 4.Multiple_variable.tf
@@ -1,12 +1,12 @@
-# Variable 1
-variable "username" {
-}
+# # Variable 1
+# variable "username" {
+# }
 
-# Variable 2
-variable "age" {
+# # Variable 2
+# variable "age" {
     
-}
+# }
 
-output "printname" {
-   value = "hello , ${var.username},your age is ${var.age}"
-}
+# output "printname" {
+#    value = "hello , ${var.username},your age is ${var.age}"
+# }
