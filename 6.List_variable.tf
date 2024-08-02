a0cc436ace83973a9f0befb395a45ee18b6e5349
Shubham <byshubham6@gmail.com>
2024-08-02 10:56:56 +0530

    #

--- 6.List_variable.tf
+++ 6.List_variable.tf
@@ -1,13 +1,13 @@
-# Variable 1 and add Variable types , list 
-variable "username" {
-    type = list(string)
-}
+# # Variable 1 and add Variable types , list 
+# variable "username" {
+#     type = list(string)
+# }
 
-# Variable 2
-variable "age" {
-    type = number
+# # Variable 2
+# variable "age" {
+#     type = number
     
-}
-output "printname" {
-   value = "hello , ${var.username[2]},your age is ${var.age}"
-}
+# }
+# output "printname" {
+#    value = "hello , ${var.username[2]},your age is ${var.age}"
+# }
