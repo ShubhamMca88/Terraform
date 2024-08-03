b6ad685982f16bd68d87a8bb9fc6f33ef87f997c
Shubham <byshubham6@gmail.com>
2024-08-03 12:28:12 +0530

    #

--- 7.Function.tf
+++ 7.Function.tf
@@ -1,9 +1,13 @@
 # Variable 1 and add Variable types , list 
 variable "username" {
     type = list(string)
-    default = ["sugam","saurabh","ashu"]
+    default = ["sugam","SAURABH","ashu"]
 }
 
 output "printname" {
-   value = "hello , ${var.username[2]},your age is ${var.age}"
+   value = "${upper(var.username[0])}"
+}
+
+output "printname" {
+   value = "${lower(var.username[1])}"
 }
