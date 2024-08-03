cf8ce98999e254fa27ef92bcc7b9eed0a9f4d8ea
Shubham <byshubham6@gmail.com>
2024-08-03 19:42:49 +0530

    #

--- 8.Map_variable.tf
+++ 8.Map_variable.tf
@@ -1,14 +1,14 @@
-# Use of Map Variable in terraform
+# # Use of Map Variable in terraform
  
-variable "username" {
-    type = map
-    default = {
-        Saurabh = 22
-        Ashu = 23
-    }
+# variable "username" {
+#     type = map
+#     default = {
+#         Saurabh = 22
+#         Ashu = 23
+#     }
     
-}
+# }
 
-output "username" {
-   value = "My name is Saurabh and my age is ${lookup(var.username ,"Saurabh")}"
-}
+# output "username" {
+#    value = "My name is Saurabh and my age is ${lookup(var.username ,"Saurabh")}"
+# }
---
+++ 9.Map_use_Dynamically.tf
@@ -0,0 +1,14 @@
+# Use of Map Variable in terraform
+ 
+variable "username" {
+    type = map 
+    default = {
+        Saurabh = 22
+        Ashu = 23
+    }
+    
+}
+
+output "username" {
+   value = "My name is Saurabh and my age is ${lookup(var.username ,"Saurabh")}"
+}
